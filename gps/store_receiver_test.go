987f8c09567dc0dc249b250cf49b5eb61821e8b5
ernestrc <ernest@unstable.build>
2020-07-30 13:33:08 -0700

    Add cache and time-series store gps.Receiver

---
+++ gps/store_receiver_test.go
@@ -0,0 +1,151 @@
+package gps
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/ernestrc/blue/datastore/document"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	c1     = fixtureCoords
+	c2     = fixtureCoords
+	c3     = fixtureCoords
+	second = time.Unix(c1.UnixTime, 0).Add(-time.Minute)
+	first  = time.Unix(c1.UnixTime, 0).Add(-5 * time.Minute)
+	c4     = Coordinates{}
+)
+
+func init() {
+	c1.UnixTime = first.Unix()
+
+	c2.UnixTime = second.Unix()
+
+	c3.DeviceID = "2"
+	c3.UnixTime = second.Unix()
+
+	dayBefore := time.Unix(fixtureCoords.UnixTime, 0).Add(-24 * time.Hour).Unix()
+	c4 = Coordinates{DeviceID: deviceID, UnixTime: dayBefore}
+}
+
+func assertIterCount(t *testing.T, it document.Iterator, expected int) []Coordinates {
+	var i int
+	var ret []Coordinates
+	for it.HasNext() {
+		var pos Coordinates
+		err := it.NextTo(&pos)
+		require.NoError(t, err)
+		require.NotZero(t, pos)
+		i++
+		ret = append(ret, pos)
+	}
+	assert.Equal(t, expected, i)
+	require.Len(t, ret, expected)
+	return ret
+}
+
+func newTestingStore(t *testing.T, opts ...Option) *Store {
+	ctx := context.Background()
+	b := document.NewInMemoryCache()
+	s := NewStore(b, opts...)
+
+	err := s.Receive(ctx, c1)
+	require.NoError(t, err)
+
+	err = s.Receive(ctx, c2)
+	require.NoError(t, err)
+
+	err = s.Receive(ctx, c3)
+	require.NoError(t, err)
+
+	return s
+}
+
+func TestStoreReceiverNoOptions(t *testing.T) {
+	ctx := context.Background()
+	s := newTestingStore(t)
+
+	t.Run("List returns gps data for all devices", func(t *testing.T) {
+		it, err := s.List(ctx, time.Time{}, time.Now())
+		require.NoError(t, err)
+		res := assertIterCount(t, it, 3)
+		assert.ElementsMatch(t, res, []Coordinates{c1, c2, c3})
+	})
+
+	t.Run("List returns gps data in a left inclusive, right exclusive fashion", func(t *testing.T) {
+		it, err := s.List(ctx, first, second)
+		require.NoError(t, err)
+		assertIterCount(t, it, 1)
+	})
+
+	t.Run("List returns empty iterator if range does not contain gps data", func(t *testing.T) {
+		it, err := s.List(ctx, time.Now(), time.Now())
+		require.NoError(t, err)
+		assertIterCount(t, it, 0)
+	})
+
+	t.Run("ListDevice returns empty iterator if range does not contain gps data", func(t *testing.T) {
+		{
+			it, err := s.ListDevice(ctx, deviceID, time.Now(), time.Now())
+			require.NoError(t, err)
+			assertIterCount(t, it, 0)
+		}
+		{
+			it, err := s.ListDevice(ctx, "blah", first, second)
+			require.NoError(t, err)
+			assertIterCount(t, it, 0)
+		}
+	})
+
+	t.Run("ListDevice returns all gps data for a device", func(t *testing.T) {
+		it, err := s.ListDevice(ctx, deviceID, time.Time{}, time.Now())
+		require.NoError(t, err)
+		res := assertIterCount(t, it, 2)
+		assert.ElementsMatch(t, []Coordinates{c1, c2}, res)
+	})
+
+	t.Run("ListDevice returns gps data in a left inclusive, right exclusive fashion", func(t *testing.T) {
+		it, err := s.ListDevice(ctx, deviceID, first, second)
+		require.NoError(t, err)
+		assertIterCount(t, it, 1)
+	})
+}
+
+func TestStoreReceiverSampling(t *testing.T) {
+	ctx := context.Background()
+	s := newTestingStore(t, WithDownSampling(time.Hour))
+
+	err := s.Receive(ctx, c4)
+	require.NoError(t, err)
+
+	t.Run("List returns gps downsampled data for all devices", func(t *testing.T) {
+		it, err := s.List(ctx, time.Time{}, time.Now())
+		require.NoError(t, err)
+		res := assertIterCount(t, it, 3)
+		assert.ElementsMatch(t, res, []Coordinates{c2, c3, c4})
+	})
+}
+
+func TestStoreReceiverFixedSize(t *testing.T) {
+	ctx := context.Background()
+	s := newTestingStore(t, WithFixedSize(1),
+		WithDownSampling(time.Minute))
+
+	err := s.Receive(ctx, c4)
+	require.NoError(t, err)
+
+	c5 := c3
+	c5.UnixTime += 100
+	err = s.Receive(ctx, c5)
+	require.NoError(t, err)
+
+	t.Run("List returns gps last N records for all devices", func(t *testing.T) {
+		it, err := s.List(ctx, time.Time{}, time.Now())
+		require.NoError(t, err)
+		res := assertIterCount(t, it, 2)
+		assert.ElementsMatch(t, res, []Coordinates{c4, c5})
+	})
+}
