82a5f5ed267f920d2a90f15bdd97783d8a3e5908
ernestrc <ernest@unstable.build>
2020-07-31 09:58:58 -0700

    Add gps.Cache.GetAll

--- gps/cache_receiver_test.go
+++ gps/cache_receiver_test.go
@@ -10,15 +10,30 @@ import (
 )
 
 func TestCacheReceiver(t *testing.T) {
+	coords := fixtureCoords
 	b := document.NewInMemoryCache()
 	c := NewCache(b)
 
 	ctx := context.Background()
 
-	err := c.Receive(ctx, fixtureCoords)
+	err := c.Receive(ctx, coords)
 	require.NoError(t, err)
 
 	pos, err := c.Get(ctx, deviceID)
 	require.NoError(t, err)
-	assert.Equal(t, fixtureCoords, pos)
+	assert.Equal(t, coords, pos)
+
+	allPos, err := c.GetAll(ctx)
+	require.NoError(t, err)
+	assert.Len(t, allPos, 1)
+	assert.Equal(t, coords, allPos[0])
+
+	coords.DeviceID = "jfkewkjl"
+	err = c.Receive(ctx, coords)
+	require.NoError(t, err)
+
+	allPos, err = c.GetAll(ctx)
+	require.NoError(t, err)
+	assert.Len(t, allPos, 2)
+	assert.Equal(t, coords, allPos[1])
 }
