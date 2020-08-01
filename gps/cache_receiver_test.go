132c6274b55897a7d78796d7e1505d1c38047821
ernestrc <ernest@unstable.build>
2020-08-01 11:44:46 -0700

    Fix gps.Cache Receive

--- gps/cache_receiver_test.go
+++ gps/cache_receiver_test.go
@@ -19,21 +19,36 @@ func TestCacheReceiver(t *testing.T) {
 	err := c.Receive(ctx, coords)
 	require.NoError(t, err)
 
-	pos, err := c.Get(ctx, deviceID)
-	require.NoError(t, err)
-	assert.Equal(t, coords, pos)
+	t.Run("Get", func(t *testing.T) {
+		pos, err := c.Get(ctx, deviceID)
+		require.NoError(t, err)
+		assert.Equal(t, coords, pos)
+	})
 
-	allPos, err := c.GetAll(ctx)
-	require.NoError(t, err)
-	assert.Len(t, allPos, 1)
-	assert.Equal(t, coords, allPos[0])
+	t.Run("GetAll", func(t *testing.T) {
+		pos := fixtureCoords
+		allPos, err := c.GetAll(ctx)
+		require.NoError(t, err)
+		assert.Len(t, allPos, 1)
+		assert.Equal(t, pos, allPos[0])
 
-	coords.DeviceID = "jfkewkjl"
-	err = c.Receive(ctx, coords)
-	require.NoError(t, err)
+		pos.DeviceID = "jfkewkjl"
+		err = c.Receive(ctx, pos)
+		require.NoError(t, err)
 
-	allPos, err = c.GetAll(ctx)
-	require.NoError(t, err)
-	assert.Len(t, allPos, 2)
-	assert.Equal(t, coords, allPos[1])
+		allPos, err = c.GetAll(ctx)
+		require.NoError(t, err)
+		assert.Len(t, allPos, 2)
+		assert.Equal(t, pos, allPos[1])
+	})
+
+	t.Run("Receive multiple times", func(t *testing.T) {
+		pos := fixtureCoords
+		pos.DeviceID = "TuTu"
+		err := c.Receive(ctx, coords)
+		require.NoError(t, err)
+
+		err = c.Receive(ctx, coords)
+		require.NoError(t, err)
+	})
 }
