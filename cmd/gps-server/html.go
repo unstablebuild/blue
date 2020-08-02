ab9ae4416987cc36f408c322a08c80c623aad364
ernestrc <ernest@unstable.build>
2020-08-01 22:26:31 -0700

    Add HTTP gps coordinates server

---
+++ cmd/gps-server/html.go
@@ -0,0 +1,52 @@
+package main
+
+const template = `
+<html>
+  <head>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js" integrity="sha512-rmZcZsyhe0/MAjquhTgiUcb4d9knaFc7b5xAfju483gbEXTkeJRUMIPk6s3ySZMYUHEcjKbjLjyddGWMrNEvZg==" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load("current", {
+        "packages":["map"],
+		'mapsApiKey': '%s'
+      });
+
+      var options = {
+        showTooltip: true,
+        showInfoWindow: true,
+        useMapTypeControl: true,
+		enableScrollWheel: true,
+	  }
+
+	  function makeLabel(o) {
+		  var lastSeen = moment.unix(o.UnixTime).fromNow();
+		  return o.DeviceID + " " + lastSeen;
+	  }
+
+	  function loadData() {
+	    $.ajax({
+	      url: "/location/devices",
+	      success: function(result) {
+	    	  var obj = JSON.parse(result);
+	    	  var rawData = obj.map(o => {
+	    	  	return [o.Latitude, o.Longitude, makeLabel(o)];
+	    	  });
+	    	  rawData = ([['Lat', 'Long', 'Name']]).concat(rawData);
+	    	  console.log(rawData)
+              var data = google.visualization.arrayToDataTable(rawData);
+              var map = new google.visualization.Map(document.getElementById('map_div'));
+              map.draw(data, options);
+	      }
+	    });
+      }
+
+      google.charts.setOnLoadCallback(loadData);
+    </script>
+  </head>
+
+  <body>
+    <div id="map_div" style="width: 100%%; height: 100%%"></div>
+  </body>
+</html>
+`
---
+++ cmd/gps-server/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/pion/dtls/v2"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/ernestrc/blue/gps"
+	"github.com/ernestrc/blue/logging"
+)
+
+var (
+	dtlsConfig = dtls.Config{
+		CipherSuites:         []dtls.CipherSuiteID{dtls.TLS_PSK_WITH_AES_128_CCM_8},
+		ExtendedMasterSecret: dtls.RequireExtendedMasterSecret,
+	}
+
+	storeConfig = gps.DefaultMultiStoreConfig()
+
+	debug           = flag.Bool("v", false, "Enable verbose logging")
+	psk             = flag.String("k", "", "PSK key file name to use for DTLS handshake")
+	pskIdentity     = flag.String("i", "GPS DTLS Server", "PSK identity hint")
+	dtlsPort        = flag.Int("P", gps.DefaultPort, "Listening DTLS port")
+	httpPort        = flag.Int("X", 8080, "Listening HTTP port")
+	host            = flag.String("h", "127.0.0.1", "Listening DTLS address")
+	boltDbPath      = flag.String("b", ".gps-boltdb", "Path to mount the Bolt DB for GPS store")
+	gcCredsFile     = flag.String("c", "", "Google Cloud credentials file fore GPS store")
+	gcProjectID     = flag.String("p", "", "Google Cloud project ID fore GPS store")
+	gmapsAPIKeyFile = flag.String("m", "", "Google Maps API Key file")
+)
+
+func parseFlags() {
+	flag.Parse()
+
+	logging.SetDefaults(*debug)
+
+	if *psk == "" {
+		log.Fatal("Must pass -k flag")
+	}
+
+	if *gcProjectID == "" {
+		log.Fatal("Must pass -p flag")
+	}
+
+	if *gmapsAPIKeyFile == "" {
+		log.Fatal("Must pass -m flag")
+	}
+
+	dtlsConfig.PSK = func(hint []byte) ([]byte, error) {
+		log.Debugf("reading PSK file %s for hint %s", *psk, string(hint))
+		return ioutil.ReadFile(*psk)
+	}
+	dtlsConfig.PSKIdentityHint = []byte(*pskIdentity)
+
+	storeConfig.Bolt.DBPath = *boltDbPath
+	storeConfig.Firestore.CredsFile = *gcCredsFile
+	storeConfig.Firestore.ProjectID = *gcProjectID
+}
+
+func loadGoogleMapsAPIKey() (string, error) {
+	raw, err := ioutil.ReadFile(*gmapsAPIKeyFile)
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSuffix(string(raw), "\n"), nil
+}
+
+func main() {
+	parseFlags()
+
+	store, err := gps.NewMultiStore(storeConfig)
+	if err != nil {
+		log.Fatalf("could not create store: %v", err)
+	}
+
+	s, err := gps.NewDTLSServer(*host, *dtlsPort, dtlsConfig, store)
+	if err != nil {
+		log.Fatalf("could not start server: %v", err)
+	}
+	defer s.Close()
+
+	addr := fmt.Sprintf(":%d", *httpPort)
+	gmapsAPIKey, err := loadGoogleMapsAPIKey()
+	if err != nil {
+		log.Fatalf("could not read google maps api key file %s: %v",
+			*gmapsAPIKeyFile, err)
+	}
+	httpHandler := logging.NewMiddleware(newAPI(store, string(gmapsAPIKey)))
+
+	go func() {
+		log.Fatal(s.Serve())
+	}()
+
+	log.Infof("HTTP server listening at address %s", addr)
+	log.Fatal(http.ListenAndServe(addr, httpHandler))
+}
