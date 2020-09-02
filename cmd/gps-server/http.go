0e865eb8ebdb38b86bee30702d578070aab6ffbd
ernestrc <ernest@unstable.build>
2020-09-01 18:24:09 -0700

    Add bluectl gps track CLI

--- cmd/gps-server/http.go
+++ cmd/gps-server/http.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/ernestrc/blue/datastore/document"
 	"github.com/ernestrc/blue/gps"
 	"github.com/julienschmidt/httprouter"
 	log "github.com/sirupsen/logrus"
@@ -51,7 +53,11 @@ func (a *api) serveDeviceLocation(
 	ctx := r.Context()
 	pos, err := a.store.GetDevice(ctx, deviceID)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		if err == document.ErrNotFound {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 		return
 	}
 
@@ -62,6 +68,50 @@ func (a *api) serveDeviceLocation(
 	}
 
 	w.Write(b)
+}
+
+func (a *api) serveDeviceTracks(
+	w http.ResponseWriter, r *http.Request, ps httprouter.Params,
+) {
+	deviceID := ps.ByName("device_id")
+	if deviceID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	it, err := a.store.ListDevice(ctx, deviceID, time.Time{}, time.Now())
+	if err != nil {
+		if err == document.ErrNotFound {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Write([]byte("["))
+	var pos gps.Coordinates
+	for it.HasNext() {
+		if pos != (gps.Coordinates{}) {
+			w.Write([]byte(","))
+		}
+		err := it.NextTo(&pos)
+		if err != nil {
+			log.Errorf("could not decode gps.Coordinates: %v", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		b, err := json.Marshal(pos)
+		if err != nil {
+			log.Errorf("could not marshal Coordinates: %v: %v", pos, err)
+			return
+		}
+		w.Write(b)
+	}
+	w.Write([]byte("]"))
+
 }
 
 func newAPI(store *gps.MultiStore, gmapsAPIKey string) *api {
@@ -77,6 +127,7 @@ func newAPI(store *gps.MultiStore, gmapsAPIKey string) *api {
 	router.GET("/map", a.serveMap)
 	router.GET("/location/devices/:device_id", a.serveDeviceLocation)
 	router.GET("/location/devices", a.serveAllDevicesLocation)
+	router.GET("/track/devices/:device_id", a.serveDeviceTracks)
 
 	return a
 }
