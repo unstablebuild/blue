c1bcf9370da4fc681b20952447d91f7fa754ad9d
ernestrc <ernest@unstable.build>
2020-09-06 21:37:57 -0700

    Add logd service gps-producer.lua

--- deploy/logd/common.lua
+++ deploy/logd/common.lua
@@ -2,7 +2,7 @@ local logd = require("logd")
 
 -- returns duration_us field in logptr, parsed as a
 -- duration number in nanoseconds.
-function parse_duration(logptr)
+local function parse_duration(logptr)
 	local d = logd.log_get(logptr, "duration_us")
 	local duration_us = tonumber(d)
 	if duration_us == nil then
@@ -12,9 +12,33 @@ function parse_duration(logptr)
 	return duration_us * 1000
 end
 
+local function init_prometheus(prom, prefix)
+	return prom.init(prefix, {
+		log = function(level, msg)
+			logd.print({
+				level = level,
+				msg = "Prometheus error: " .. msg,
+			})
+		end
+	})
+end
+
+function logd.on_error(msg, logptr, at)
+	logs:inc(1, {'failure'})
+
+	logd.print({
+		level = 'ERROR',
+		err = msg,
+		at = at,
+		partial = logd.to_str(logptr),
+	})
+end
+
 return {
 	latency_buckets = {
 		5000, 100000, 500000, 1000000, 5000000, 10000000,
 		50000000, 100000000, 500000000, 1000000000, 5000000000,
-	}
+	},
+	init_prometheus = init_prometheus,
+	parse_duration = parse_duration,
 }
---
+++ deploy/logd/gps-producer.lua
@@ -0,0 +1,83 @@
+--
+-- This logd script is designed to consume logs from gps-producer
+-- Before running, make sure you install all the dependencies via `lit install`.
+--
+local logd = require('logd')
+local http = require('http')
+local common = require('common')
+local prometheus_prefix = 'gps_producer_'
+
+-- init prometheus module
+local prometheus = common.init_prometheus(require('prometheus'), prometheus_prefix)
+
+local logs = prometheus:counter('logs', 'Total logs processed counter', {'status'})
+local errors = prometheus:counter('errors', 'Total errors either as ERROR level or failure step', {'class', 'callType'})
+local position_data = prometheus:gauge('gps_data',
+	'GPS position by "metric" (Altitude, Latitude or Longitude)', {'metric'})
+local position_lat = prometheus:histogram('position_latency_ns',
+	'Latency of successful gps.PositionerPosition in nanoseconds', nil,
+	common.latency_buckets)
+
+local function record_position(logptr)
+	local alt_raw = logd.log_get(logptr, 'Altitude')
+	if alt_raw ~= nil then
+		position_data:inc(tonumber(alt_raw), {'Altitude'})
+	end
+
+	local lat_raw = logd.log_get(logptr, 'Latitude')
+	if lat_raw ~= nil then
+		position_data:inc(tonumber(lat_raw), {'Latitude'})
+	end
+
+	local lng_raw = logd.log_get(logptr, 'Longitude')
+	if lng_raw ~= nil then
+		position_data:inc(tonumber(lng_raw), {'Longitude'})
+	end
+end
+
+local function record_position_lat(logptr, duration)
+	position_lat:observe(duration)
+end
+
+function logd.on_log(logptr)
+	logs:inc(1, {'success'})
+		
+	local k = logd.log_get(logptr, 'class')
+	local ct = logd.log_get(logptr, 'callType')
+	local l = logd.log_get(logptr, 'level')
+	local s = logd.log_get(logptr, 'step')
+
+	if l == 'ERROR' or s == 'failure' then
+		errors:inc(1, {k, ct})
+		return
+	end
+
+	if s ~= 'success' then
+		return
+	end
+
+	local duration = common.parse_duration(logptr)
+	if duration == nil then
+		return
+	end
+
+	if ct == 'gps.PositionerPosition' then
+		record_position(logptr)
+		record_position_lat(logptr, duration)
+	end
+end
+
+-- collect metrics and satisfy request
+local function on_metrics_request(req, res)
+	local body = prometheus:collect()
+	res:setHeader("Content-Type", "text/plain")
+	res:setHeader("Content-Length", #body)
+	res:finish(body)
+end
+
+local function setup_metrics_server(port)
+	http.createServer(on_metrics_request):listen(port)
+	logd.print("Prometheus exporter listening at http://localhost:8080/")
+end
+
+setup_metrics_server(8080)
