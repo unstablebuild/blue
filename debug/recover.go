b9cd777f37143d31cb9f4cab31f279bd5a50de78
ernestrc <ernest@unstable.build>
2022-10-18 11:17:13 -0700

    Add debug.CapturePanic to collect panic reports

---
+++ debug/recover.go
@@ -0,0 +1,56 @@
+package debug
+
+import (
+	"fmt"
+	"runtime/debug"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// PanicReport contains informatin about a panic
+type PanicReport struct {
+	Package string
+	Version string
+	debug.BuildInfo
+	Error     string
+	Stack     string
+	CreatedAt time.Time
+}
+
+// CapturePanic attempts to capture a panic during execution of f, logs it
+// and returns a PanicReport and false, or returns true if f returned
+// successfully.
+func CapturePanic(log *log.Logger, pkg, version string, f func()) (ok bool, report PanicReport) {
+	defer func() {
+		r := recover()
+		if r == nil {
+			return
+		}
+		report = PanicReport{
+			Package:   pkg,
+			Version:   version,
+			Stack:     string(debug.Stack()),
+			CreatedAt: time.Now(),
+		}
+		bi, ok := debug.ReadBuildInfo()
+		if ok {
+			report.BuildInfo = *bi
+		}
+		switch x := r.(type) {
+		case string:
+			report.Error = x
+		case error:
+			report.Error = x.Error()
+		default:
+			report.Error = fmt.Sprintf("unknown: %v", r)
+		}
+
+		log.Errorf("CapturePanic: panic: %v", report.Error)
+		ok = false
+	}()
+
+	f()
+	ok = true
+	return
+}
