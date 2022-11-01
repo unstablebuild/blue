a58893a5f29d14116b09609fa0bf61831bf36394
ernestrc <ernest@unstable.build>
2022-11-01 15:38:16 -0700

    Update firstmover.Service to handle permission denied

---
+++ document/firstmover/err_service.go
@@ -0,0 +1,39 @@
+package firstmover
+
+import (
+	"context"
+
+	"github.com/ernestrc/blue/document"
+)
+
+type errService struct {
+	err error
+}
+
+func (e errService) Create(ctx context.Context, ID string, doc interface{}) error {
+	return e.err
+}
+
+func (e errService) Set(ctx context.Context, ID string, doc interface{}) error {
+	return e.err
+}
+
+func (e errService) Update(ctx context.Context, ID string, updates []document.Update) error {
+	return e.err
+}
+
+func (e errService) Get(ctx context.Context, ID string, doc interface{}) error {
+	return e.err
+}
+
+func (e errService) Delete(ctx context.Context, ID string) error {
+	return e.err
+}
+
+func (e errService) List(ctx context.Context, filters []document.Filter) (document.Iterator, error) {
+	return nil, e.err
+}
+
+func (e errService) Close() error {
+	return nil
+}
