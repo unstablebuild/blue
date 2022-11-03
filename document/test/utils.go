a93d2a661c0aa68286d712a41fc30ffb65b1acde
ernestrc <ernest@unstable.build>
2022-11-03 01:46:51 -0700

    Remove struct tags from document test suite
    Now that clients can control whether encoding is lowercase or not
    there's no need to preemptively set those.

--- document/test/utils.go
+++ document/test/utils.go
@@ -21,8 +21,8 @@ type Reaper interface {
 }
 
 type Segador struct {
-	Name          string                 `json:"name" bson:"name" yaml:"name" toml:"name"`
-	Traits        map[string]interface{} `json:"traits" bson:"traits" yaml:"traits" toml:"traits"`
+	Name          string
+	Traits        map[string]interface{}
 	internalField string
 }
 
@@ -93,7 +93,7 @@ var (
 )
 
 type myOtherEntity struct {
-	Value     int       `json:"value" bson:"value" yaml:"value" toml:"value"`
+	Value     int
 	UpdatedAt time.Time `firestore:",serverTimestamp"`
 	CreatedAt time.Time `firestore:",serverTimestamp"`
 }
