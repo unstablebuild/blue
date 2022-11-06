663e5d065b402ca08b4a528d25ed292a7ac2e4ce
ernestrc <ernest@unstable.build>
2022-11-06 07:48:38 -0800

    Update bluectl temp issue flow to handle yaml errors

--- cmd/bluectl/issue/util.go
+++ cmd/bluectl/issue/util.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ernestrc/blue/issue"
 	"github.com/ernestrc/sensible/editor"
 	log "github.com/sirupsen/logrus"
+	"go.uber.org/multierr"
 	"gopkg.in/yaml.v3"
 )
 
@@ -27,35 +28,38 @@ func getDefaultAuthor() string {
 }
 
 func tempIssue(ret issue.Report, defaultAuthor string) (issue.Report, error) {
+	f, err := ioutil.TempFile("", "blue-issue")
+	if err != nil {
+		err = fmt.Errorf("failed create temp file: %v", err)
+		return issue.Report{}, err
+	}
+
+	dataIn, err := yaml.Marshal(&ret)
+	if err != nil {
+		err = fmt.Errorf("failed to marshal issue.Report into yaml: %v", err)
+		return issue.Report{}, err
+	}
+
+	_, err = f.Write(dataIn)
+	if err != nil {
+		err = fmt.Errorf("failed write data to temp file: %v", err)
+		return issue.Report{}, err
+	}
+
+	err = f.Sync()
+	if err != nil {
+		err = fmt.Errorf("failed sync data to temp file: %v", err)
+		return issue.Report{}, err
+	}
+
+	// reset report so deleted fields are reset, for instance
+	// in case this is an edit
+	ret = issue.Report{}
 
 	for {
-		f, err := ioutil.TempFile("", "blue-issue")
-		if err != nil {
-			err = fmt.Errorf("failed create temp file: %v", err)
-			return issue.Report{}, err
-		}
-
-		dataIn, err := yaml.Marshal(&ret)
-		if err != nil {
-			err = fmt.Errorf("failed to marshal issue.Report into yaml: %v", err)
-			return issue.Report{}, err
-		}
-
-		_, err = f.Write(dataIn)
-		if err != nil {
-			err = fmt.Errorf("failed write data to temp file: %v", err)
-			return issue.Report{}, err
-		}
-
 		err = editor.Edit(f)
 		if err != nil {
 			err = fmt.Errorf("failed to edit manifest: %v", err)
-			return issue.Report{}, err
-		}
-
-		err = f.Close()
-		if err != nil {
-			err = fmt.Errorf("failed to close temp file: %v", err)
 			return issue.Report{}, err
 		}
 
@@ -65,29 +69,41 @@ func tempIssue(ret issue.Report, defaultAuthor string) (issue.Report, error) {
 			return issue.Report{}, err
 		}
 
-		ret = issue.Report{}
-		err = yaml.Unmarshal(data, &ret)
-		if err != nil {
-			err = fmt.Errorf("failed decode yaml from temp file: %v", err)
-			return issue.Report{}, err
+		if string(data) == string(dataIn) {
+			err = fmt.Errorf("Issue was not modified. ")
+		} else {
+			err = yaml.Unmarshal(data, &ret)
+			if err != nil {
+				err = fmt.Errorf("Failed to decode yaml from temp file: %v. ", err)
+			} else {
+				log.Debugf("decoded issue manifest from temp file: %#v", ret)
+				if ret.Package == "" || ret.Subject == "" || ret.Author == "" {
+					err = multierr.Append(err, fmt.Errorf("Missing one or more manatory fields: "+
+						"package, subject or author. "))
+				}
+			}
 		}
 
-		log.Debugf("decoded issue manifest from temp file: %#v", ret)
-
-		if ret.Package != "" && ret.Subject != "" && ret.Author != "" {
+		if err == nil {
 			break
 		}
+
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Missing mandatory fields: package, subject and author. " +
+		fmt.Println(err.Error() +
 			"Do you want to edit the issue? [y/n]:")
 		text, _ := reader.ReadString('\n')
 		switch text {
 		case "y\n", "Y\n":
 			continue
 		default:
-			return issue.Report{}, errors.New("could not submit issue")
+			return issue.Report{}, errors.New("Did not submit issue")
 		}
 	}
 
+	err = f.Close()
+	if err != nil {
+		err = fmt.Errorf("failed to close temp file: %v", err)
+		return issue.Report{}, err
+	}
 	return ret, nil
 }
