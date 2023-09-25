0fb002e2ec722c49c259ee2218533978854bd47c
ernestrc <ernest@unstable.build>
2023-09-25 14:54:07 -0700

    auth: Update Granter to pass ProviderClaims
    Does not need to make any assumptions as to what fields are used
    to grant an auth scope.

--- auth/granter.go
+++ auth/granter.go
@@ -5,18 +5,18 @@ import "context"
 // Granter abstracts the ability to grant claims to a user
 // based on its userID and email.
 type Granter[T any] interface {
-	Grant(ctx context.Context, userID, email string) (T, error)
+	Grant(context.Context, *ProviderClaims) (T, error)
 }
 
 // FuncGranter uses fn to satisfy Granter.
-func FuncGranter[T any](fn func(context.Context, string, string) (T, error)) Granter[T] {
+func FuncGranter[T any](fn func(context.Context, *ProviderClaims) (T, error)) Granter[T] {
 	return fnGranter[T]{fn: fn}
 }
 
 type fnGranter[T any] struct {
-	fn func(context.Context, string, string) (T, error)
+	fn func(context.Context, *ProviderClaims) (T, error)
 }
 
-func (f fnGranter[T]) Grant(ctx context.Context, userID, email string) (T, error) {
-	return f.fn(ctx, userID, email)
+func (f fnGranter[T]) Grant(ctx context.Context, claims *ProviderClaims) (T, error) {
+	return f.fn(ctx, claims)
 }
