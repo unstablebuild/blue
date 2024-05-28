6e14b3163820d15105465f6828c03a5ecb454785
ernestrc <ernest@unstable.build>
2024-05-28 15:00:21 -0700

    Move firestore emulator utils to its own file

---
+++ document/firestore/emulator.go
@@ -0,0 +1,59 @@
+package firestore
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/unstablebuild/blue/logging"
+	"github.com/unstablebuild/blue/logging/trace"
+)
+
+// RunFirestoreEmulator runs firestore emulator as a subprocess.
+//
+// It sets FIRESTORE_EMULATOR_HOST so calls to firestore.NewClient
+// know that emulator is to be used instead of real Firestore.
+//
+// Gloud SDK and the emulator need to be installed first.
+func RunFirestoreEmulator() (teardown func() error, err error) {
+	const addr = "0.0.0.0:8045"
+	const callType = "StartFirestoreEmulator"
+
+	cmd := exec.Command("gcloud", "beta", "emulators", "firestore", "start", fmt.Sprintf("--host-port=%s", addr))
+	err = cmd.Start()
+	if err != nil {
+		return
+	}
+
+	traceID := trace.New()
+	field := logging.Field{Key: "address", Value: addr}
+	start := logging.LogAttempt(traceID, callType, field)
+
+	teardown = cmd.Process.Kill
+	err = waitForEmulator(addr)
+	if err != nil {
+		teardown()
+	} else {
+		os.Setenv("FIRESTORE_EMULATOR_HOST", addr)
+	}
+
+	logging.LogResult(err, start, traceID, callType, field)
+	return
+}
+
+func waitForEmulator(addr string) (err error) {
+	const tries = 20
+	const backoff = 500 * time.Millisecond
+	var conn net.Conn
+	for i := 0; i < tries; i++ {
+		conn, err = net.Dial("tcp", addr)
+		if err == nil {
+			conn.Close()
+			return
+		}
+		time.Sleep(backoff)
+	}
+	return
+}
--- document/firestore/service.go
+++ document/firestore/service.go
@@ -3,18 +3,13 @@ package firestore
 import (
 	"context"
 	"errors"
-	"fmt"
-	"net"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 
 	"cloud.google.com/go/firestore"
-	"github.com/unstablebuild/blue/document"
-	"github.com/unstablebuild/blue/logging"
-	"github.com/unstablebuild/blue/logging/trace"
 	"github.com/sirupsen/logrus"
+	"github.com/unstablebuild/blue/document"
 	"google.golang.org/api/iterator"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -236,51 +231,4 @@ func convertError(err error) error {
 		logrus.Warningf("err %s: code %v", err, status.Code(err))
 	}
 	return err
-}
-
-func waitForEmulator(addr string) (err error) {
-	const tries = 20
-	const backoff = 500 * time.Millisecond
-	var conn net.Conn
-	for i := 0; i < tries; i++ {
-		conn, err = net.Dial("tcp", addr)
-		if err == nil {
-			conn.Close()
-			return
-		}
-		time.Sleep(backoff)
-	}
-	return
-}
-
-// RunFirestoreEmulator runs firestore emulator as a subprocess.
-//
-// It sets FIRESTORE_EMULATOR_HOST so calls to firestore.NewClient
-// know that emulator is to be used instead of real Firestore.
-//
-// Gloud SDK and the emulator need to be installed first.
-func RunFirestoreEmulator() (teardown func() error, err error) {
-	const addr = "0.0.0.0:8045"
-	const callType = "StartFirestoreEmulator"
-
-	cmd := exec.Command("gcloud", "beta", "emulators", "firestore", "start", fmt.Sprintf("--host-port=%s", addr))
-	err = cmd.Start()
-	if err != nil {
-		return
-	}
-
-	traceID := trace.New()
-	field := logging.Field{Key: "address", Value: addr}
-	start := logging.LogAttempt(traceID, callType, field)
-
-	teardown = cmd.Process.Kill
-	err = waitForEmulator(addr)
-	if err != nil {
-		teardown()
-	} else {
-		os.Setenv("FIRESTORE_EMULATOR_HOST", addr)
-	}
-
-	logging.LogResult(err, start, traceID, callType, field)
-	return
 }
