8a6b9db0428264cb0c3c9876fad7b9a250186ac2
ernestrc <ernest@unstable.build>
2024-09-18 15:43:16 -0700

    Add iterator.FromStream
    Wraps a grpc.ClientStream into an iterator

---
+++ iterator/grpc_test.go
@@ -0,0 +1,131 @@
+// Unstable Build LLC ("COMPANY") CONFIDENTIAL
+//
+// Unpublished Copyright (c) 2018-2024 Unstable Build, All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of COMPANY.
+// The intellectual and technical concepts contained herein are proprietary to
+// COMPANY and may be covered by U.S. and Foreign Patents, patents in process,
+// and are protected by trade secret or copyright law. Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written permission
+// is obtained from COMPANY. Access to the source code contained herein is hereby
+// forbidden to anyone except current COMPANY employees, managers or contractors who
+// have executed Confidentiality and Non-disclosure agreements explicitly covering such access.
+//
+// The copyright notice above does not evidence any actual or intended publication or
+// disclosure of this source code, which includes information that is confidential and/or
+// proprietary, and is a trade secret, of COMPANY. ANY REPRODUCTION, MODIFICATION,
+// DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE
+// WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN
+// VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF
+// THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
+// REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL
+// ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+
+package iterator
+
+import (
+	"context"
+	"errors"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+)
+
+type data struct {
+	X string
+}
+
+func TestStreamIterator(t *testing.T) {
+	goleak.VerifyNone(t)
+
+	t.Run("unblocks Next if context is canceled", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		go func() {
+			time.Sleep(50 * time.Millisecond)
+			cancel()
+		}()
+		stream := blockingStream{quitCh: make(chan struct{})}
+		it := FromStream[data](ctx, func() {
+			close(stream.quitCh)
+			cancel()
+		}, stream)
+		_, ok := it.Next(ctx)
+		assert.False(t, ok)
+		assert.Error(t, it.Err())
+	})
+
+	t.Run("unblocks Next if Close is called", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(context.Background())
+		stream := blockingStream{quitCh: make(chan struct{})}
+		it := FromStream[data](ctx, func() {
+			close(stream.quitCh)
+			cancel()
+		}, stream)
+		go func() {
+			_ = it.Close()
+		}()
+		_, ok := it.Next(ctx)
+		assert.False(t, ok)
+		assert.Error(t, it.Err())
+	})
+
+	t.Run("exhausts underlying document iterator", func(t *testing.T) {
+		a, b, c := data{X: "a"}, data{X: "b"}, data{X: "c"}
+		stream := grpcClientStream{data: []data{a, b, c}}
+		var cancelCalled bool
+		it := FromStream[data](context.Background(), func() {
+			cancelCalled = true
+		}, &stream)
+		actual, err := Reduce(context.Background(), it,
+			func(ret []data, t *data) ([]data, error) {
+				return append(ret, *t), nil
+			})
+		require.NoError(t, err)
+		assert.EqualValues(t, []data{a, b, c}, actual)
+
+		err = it.Close()
+		require.NoError(t, err)
+		assert.True(t, cancelCalled)
+	})
+
+	t.Run("bubbles up underlying document iterator errors", func(t *testing.T) {
+		dit := FromStream[data](context.Background(), func() {}, &errorStream{})
+		_, ok := dit.Next(context.Background())
+		assert.False(t, ok)
+		assert.EqualError(t, dit.Err(), "1 error occurred: kaboom")
+	})
+}
+
+type grpcClientStream struct {
+	data []data
+}
+
+func (b *grpcClientStream) RecvMsg(doc interface{}) error {
+	if len(b.data) == 0 {
+		return io.EOF
+	}
+	ptr := doc.(*data)
+	*ptr = b.data[0]
+	b.data = b.data[1:]
+	return nil
+}
+
+type blockingStream struct {
+	quitCh chan struct{}
+}
+
+func (b blockingStream) RecvMsg(doc interface{}) error {
+	<-b.quitCh
+	return nil
+}
+
+type errorStream struct {
+}
+
+func (b *errorStream) RecvMsg(doc interface{}) error {
+	return errors.New("kaboom")
+}
