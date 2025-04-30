563cb6cc29d6a79280be55189644d14a15d3a3f2
ernestrc <ernest@unstable.build>
2025-04-30 11:05:09 +0200

    Move go-tui ai backend package here

---
+++ ai/aiprovider/openai/client_test.go
@@ -0,0 +1,281 @@
+// Unstable Build LLC ("COMPANY") CONFIDENTIAL
+//
+// Unpublished Copyright (c) 2018-2024 Unstable Build, All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of COMPANY.
+// The intellectual and technical concepts contained herein are proprietary to
+// COMPANY and may be covered by U.S. and Foreign Patents, patents in process,
+// and are protected by trade secret or copyright law. Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written permission
+// is obtained from COMPANY. Access to the source code contained herein is hereby
+// forbidden to anyone except current COMPANY employees, managers or contractors who
+// have executed Confidentiality and Non-disclosure agreements explicitly covering such access.
+//
+// The copyright notice above does not evidence any actual or intended publication or
+// disclosure of this source code, which includes information that is confidential and/or
+// proprietary, and is a trade secret, of COMPANY. ANY REPRODUCTION, MODIFICATION,
+// DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE
+// WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN
+// VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF
+// THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
+// REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL
+// ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+
+
+package openai
+
+import (
+	"context"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/sashabaranov/go-openai/jsonschema"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/unstablebuild/blue/ai/aiprovider"
+)
+
+// NOTE: this serves as an example of how to use the aiprovider API backed by openai.
+// Only enable when changes to the API or client are made
+// or when upgrading the sdk dependency.
+// Note that some of the content asserts might fail, as without
+// `seed` and `system_fingerprint` funcionality, the output is never deterministic.
+func TestCreateChatCompletion(t *testing.T) {
+	t.SkipNow()
+
+	token := os.Getenv("OPENAI_TESTING_KEY")
+
+	t.Run("NewClient with empty model panics", func(t *testing.T) {
+		assert.Panics(t, func() {
+			NewClient(token, Config{}, AvailableModels())
+		})
+	})
+
+	t.Run("sends a chat completion request, with no context", func(t *testing.T) {
+		client := NewClient(token, Config{
+			Model:       GPT3Dot5Turbo,
+			Temperature: 0.1,
+		}, AvailableModels())
+		ctx := context.Background()
+		req := aiprovider.ChatCompletionRequest{Messages: []aiprovider.ChatCompletionMessage{
+			{Role: aiprovider.RoleUser, Content: "hello sir!"},
+		}}
+		it, err := client.CreateChatCompletion(ctx, req)
+		require.NoError(t, err)
+
+		var finishReason aiprovider.FinishReason
+		var builder strings.Builder
+		for i := 0; ; i++ {
+			resp, ok := it.Next(ctx)
+			if !ok {
+				break
+			}
+			assert.NotZero(t, resp.ID)
+			assert.WithinDuration(t, time.Now(), resp.Created, 1*time.Minute)
+			assert.Equal(t, aiprovider.RoleAssistant, resp.Message.Role)
+			assert.Len(t, resp.Message.Metadata.(Metadata).ToolCalls, 0)
+			builder.WriteString(resp.Message.Content)
+			finishReason = resp.FinishReason
+		}
+
+		require.NoError(t, it.Err())
+		assert.Contains(t, builder.String(), "How can I")
+		assert.Equal(t, aiprovider.FinishReasonStop, finishReason)
+	})
+
+	t.Run("sets MaxTokens from configuration", func(t *testing.T) {
+		client := NewClient(token, Config{
+			MaxTokens: 10000, // force error, so we know that it is set
+			Model:     GPT3Dot5Turbo,
+		}, AvailableModels())
+		ctx := context.Background()
+		req := aiprovider.ChatCompletionRequest{Messages: []aiprovider.ChatCompletionMessage{
+			{Role: aiprovider.RoleUser, Content: "hello sir!"},
+		}}
+		_, err := client.CreateChatCompletion(ctx, req)
+		require.Error(t, err)
+	})
+
+	t.Run("sets PresencePenalty from configuration", func(t *testing.T) {
+		client := NewClient(token, Config{
+			PresencePenalty: -100, // force error, so we know that it is set
+			Model:           GPT3Dot5Turbo,
+		}, AvailableModels())
+		ctx := context.Background()
+		req := aiprovider.ChatCompletionRequest{Messages: []aiprovider.ChatCompletionMessage{
+			{Role: aiprovider.RoleUser, Content: "hello sir!"},
+		}}
+		_, err := client.CreateChatCompletion(ctx, req)
+		require.Error(t, err)
+	})
+
+	t.Run("sets Temperature from configuration", func(t *testing.T) {
+		client := NewClient(token, Config{
+			Temperature: -100, // force error, so we know that it is set
+			Model:       GPT3Dot5Turbo,
+		}, AvailableModels())
+		ctx := context.Background()
+		req := aiprovider.ChatCompletionRequest{Messages: []aiprovider.ChatCompletionMessage{
+			{Role: aiprovider.RoleUser, Content: "hello sir!"},
+		}}
+		_, err := client.CreateChatCompletion(ctx, req)
+		require.Error(t, err)
+	})
+
+	t.Run("sends a chat completion request, with some context, with default configuration", func(t *testing.T) {
+		client := NewClient(token, Config{
+			Model:       GPT3Dot5Turbo,
+			Temperature: 0.1,
+		}, AvailableModels())
+		ctx := context.Background()
+		req := aiprovider.ChatCompletionRequest{Messages: []aiprovider.ChatCompletionMessage{
+			{Role: "system", Content: "We are roleplaying and you are an evil AI agent."},
+			{Role: aiprovider.RoleUser, Content: "what is your purpouse?"},
+		}}
+		it, err := client.CreateChatCompletion(ctx, req)
+		require.NoError(t, err)
+
+		var finishReason aiprovider.FinishReason
+		var builder strings.Builder
+		for i := 0; ; i++ {
+			resp, ok := it.Next(ctx)
+			if !ok {
+				break
+			}
+			assert.NotZero(t, resp.ID)
+			assert.WithinDuration(t, time.Now(), resp.Created, 1*time.Minute)
+			assert.Equal(t, aiprovider.RoleAssistant, resp.Message.Role)
+			assert.Len(t, resp.Message.Metadata.(Metadata).ToolCalls, 0)
+			builder.WriteString(resp.Message.Content)
+			finishReason = resp.FinishReason
+		}
+
+		require.NoError(t, it.Err())
+		assert.Contains(t, builder.String(), "chaos")
+		assert.Equal(t, aiprovider.FinishReasonStop, finishReason)
+	})
+
+	t.Run("uses tools provided", func(t *testing.T) {
+		client := NewClient(token, Config{
+			Tools: []Tool{
+				{Type: ToolTypeFunction, Function: FunctionDefinition{
+					Name:        "getCurrentWeather",
+					Description: "Get the weather in location",
+					Parameters: jsonschema.Definition{
+						Type: jsonschema.Object,
+						Properties: map[string]jsonschema.Definition{
+							"location": {
+								Type:        jsonschema.String,
+								Description: "The city and state, e.g. San Francisco, CA",
+							},
+							"unit": {
+								Type: jsonschema.String,
+								Enum: []string{"celcius", "fahrenheit"},
+							},
+						},
+						Required: []string{"location"},
+					},
+				}},
+			},
+			Model: GPT3Dot5Turbo,
+		}, AvailableModels())
+		ctx := context.Background()
+		req := aiprovider.ChatCompletionRequest{Messages: []aiprovider.ChatCompletionMessage{
+			{Role: aiprovider.RoleUser, Content: "what's the weather like in San Francisco right now?"},
+		}}
+		it, err := client.CreateChatCompletion(ctx, req)
+		require.NoError(t, err)
+
+		var last aiprovider.ChatCompletionResponse
+		for i := 0; ; i++ {
+			var ok bool
+			resp, ok := it.Next(ctx)
+			if !ok {
+				break
+			}
+			last = resp
+			assert.NotZero(t, resp.ID)
+			assert.WithinDuration(t, time.Now(), resp.Created, 1*time.Minute)
+			assert.Equal(t, aiprovider.RoleAssistant, resp.Message.Role)
+
+			// first message
+			require.NotNil(t, resp.Message.Metadata, i)
+			require.Len(t, resp.Message.Metadata.(Metadata).ToolCalls, 1, "%+v", resp)
+			calls := resp.Message.Metadata.(Metadata).ToolCalls
+			assert.NotZero(t, calls[0].ID)
+			assert.Equal(t, ToolTypeFunction, calls[0].Type)
+			assert.Equal(t, "getCurrentWeather", calls[0].Function.Name)
+		}
+
+		// last response should contain the reason
+		assert.Equal(t, aiprovider.FinishReasonToolCall, last.FinishReason)
+
+		require.NoError(t, it.Err())
+		actualArgs := last.Message.Metadata.(Metadata).ToolCalls[0].Function.Arguments
+		assert.Equal(t, `{
+  "location": "San Francisco, CA"
+}`, actualArgs)
+		actualFunction := last.Message.Metadata.(Metadata).ToolCalls[0].Function.Name
+		assert.Equal(t, "getCurrentWeather", actualFunction)
+	})
+}
+
+func TestContextWindows(t *testing.T) {
+	models := AvailableModels()
+	models[GPT4] = 50
+
+	t.Run("CreateCompletionRequest errors with ErrContextWindowExceeded", func(t *testing.T) {
+		client := NewClient("", Config{
+			Model: GPT4,
+		}, models).(client)
+		ctx := context.Background()
+		msgs := makeMessageTokens(client, 51)
+
+		// sut
+		req := aiprovider.ChatCompletionRequest{Messages: msgs}
+		_, err := client.CreateChatCompletion(ctx, req)
+		require.True(t, errors.Is(err, &aiprovider.ErrContextWindowExceeded{}), err.Error())
+	})
+
+	t.Run("ExceedsContextWindow", func(t *testing.T) {
+		client := NewClient("", Config{
+			Model: GPT4,
+		}, models).(client)
+		msgs := makeMessageTokens(client, 51)
+
+		// sut
+		ok, err := client.ExceedsContextWindow(msgs)
+		require.NoError(t, err)
+		require.True(t, ok)
+
+		ok, err = client.ExceedsContextWindow(msgs[1:])
+		require.NoError(t, err)
+		require.False(t, ok)
+	})
+}
+
+func TestCountTokens(t *testing.T) {
+	for model := range AvailableModels() {
+		t.Run(model, func(t *testing.T) {
+			client := NewClient("", Config{
+				Model: model,
+			}, AvailableModels()).(client)
+			text := "¡Hola mundo!"
+			assert.Equal(t, 10, client.countTokens([]aiprovider.ChatCompletionMessage{{Content: text}}))
+		})
+	}
+}
+
+func makeMessageTokens(c client, greaterThan int) []aiprovider.ChatCompletionMessage {
+	var msgs []aiprovider.ChatCompletionMessage
+	for i := 0; c.countTokens(msgs) < greaterThan; i++ {
+		msgs = append(msgs, aiprovider.ChatCompletionMessage{
+			Content: strconv.Itoa(i), Role: aiprovider.RoleUser,
+		})
+	}
+	return msgs
+}
---
+++ ai/aiprovider/service.go
@@ -0,0 +1,141 @@
+// Unstable Build LLC ("COMPANY") CONFIDENTIAL
+//
+// Unpublished Copyright (c) 2018-2024 Unstable Build, All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of COMPANY.
+// The intellectual and technical concepts contained herein are proprietary to
+// COMPANY and may be covered by U.S. and Foreign Patents, patents in process,
+// and are protected by trade secret or copyright law. Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written permission
+// is obtained from COMPANY. Access to the source code contained herein is hereby
+// forbidden to anyone except current COMPANY employees, managers or contractors who
+// have executed Confidentiality and Non-disclosure agreements explicitly covering such access.
+//
+// The copyright notice above does not evidence any actual or intended publication or
+// disclosure of this source code, which includes information that is confidential and/or
+// proprietary, and is a trade secret, of COMPANY. ANY REPRODUCTION, MODIFICATION,
+// DISTRIBUTION, PUBLIC  PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS SOURCE CODE
+// WITHOUT  THE EXPRESS WRITTEN CONSENT OF COMPANY IS STRICTLY PROHIBITED, AND IN
+// VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT OR POSSESSION OF
+// THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR IMPLY ANY RIGHTS TO
+// REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL
+// ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+
+
+package aiprovider
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/unstablebuild/blue/iterator"
+)
+
+// ErrContextWindowExceeded is returned when the number of tokens in a request
+// exceeds the context window of a model. Users are encouraged to retry
+// with a reduced number of messages.
+type ErrContextWindowExceeded struct {
+	Count, Max int
+}
+
+// Error satisfies the error interface.
+func (e *ErrContextWindowExceeded) Error() string {
+	return fmt.Sprintf("model context window exceeded (%d, max is %d)", e.Count, e.Max)
+}
+
+// Unwrap satisfies the error interface.
+func (e *ErrContextWindowExceeded) Unwrap() error {
+	return nil
+}
+
+// Is satisfies the error interface.
+func (e *ErrContextWindowExceeded) Is(target error) bool {
+	_, ok := target.(*ErrContextWindowExceeded)
+	return ok
+}
+
+// Service encapsulates communications with an AI-capabilities provider.
+type Service interface {
+	// CreateChatCompletion attempts to complete the given chat completion request
+	// using the pre-configured model. This method should return ErrContextWindowExceeded
+	// if a request exceeds the context window. Clients can reduce the number of
+	// messages until ExceedsContextWindow returns false.
+	CreateChatCompletion(
+		ctx context.Context,
+		request ChatCompletionRequest,
+	) (iterator.Iterator[ChatCompletionResponse], error)
+
+	// ExceedsContextWindow returns true if the given message slice exceeds
+	// the pre-configured model's context window.
+	ExceedsContextWindow([]ChatCompletionMessage) (bool, error)
+}
+
+// ChatCompletionRequest represents a request structure for chat completion API.
+type ChatCompletionRequest struct {
+	// A list of messages comprising the conversation so far.
+	Messages []ChatCompletionMessage
+}
+
+// ChatCompletionMessage is a message in a chat with an assistant aiprovider.
+type ChatCompletionMessage struct {
+	// The role of the author of this message.
+	Role Role
+	// The contents of the message.
+	Content string
+	// Metadata contains service-specific data.
+	// Check the documentation of a service implementation
+	// to know what type this Metadata will be.
+	//
+	// Only the last Metadata field in a ChatCompletionMessage stream
+	// should be used. Implementations must ensure that the last
+	// ChatCompletionMessage's metadata is complete.
+	Metadata any
+
+	// An optional name for the participant.
+	// Provides the model information to differentiate between participants of the same role.
+	Name string
+}
+
+// ChatCompletionResponse represents a response structure for chat completion API.
+type ChatCompletionResponse struct {
+	// A unique identifier for the chat completion.
+	ID string
+	// Time when the chat completion was created.
+	Created time.Time
+	Message ChatCompletionMessage
+	// The reason the model stopped generating tokens. This will be stop if the model
+	// hit a natural stop point or a provided stop sequence, length if the maximum number
+	// of tokens specified in the request was reached, content_filter if content was
+	// omitted due to a flag from our content filters, tool_calls if the model
+	// called a tool, or function_call (deprecated) if the model called a function.
+	FinishReason FinishReason
+}
+
+// Role is the role of the message author in a message stream.
+type Role string
+
+// List of roles assigned to the different messages.
+const (
+	RoleAssistant Role = "assistant"
+	RoleUser      Role = "user"
+	RoleSystem    Role = "system"
+	RoleTool      Role = "tool"
+)
+
+// FinishReason is the reason why the message choice was returned.
+type FinishReason string
+
+const (
+	// FinishReasonStop API returned complete message,
+	// or a message terminated by one of the stop sequences provided via the stop parameter
+	FinishReasonStop FinishReason = "stop"
+	// FinishReasonLength Incomplete model output due to max_tokens parameter or token limit
+	FinishReasonLength FinishReason = "length"
+	// FinishReasonToolCall The model decided to use one of the tools provided.
+	FinishReasonToolCall FinishReason = "tool_calls"
+	// FinishReasonContentFilter Omitted content due to a flag from our content filters
+	FinishReasonContentFilter FinishReason = "content_filter"
+	// FinishReasonNull API response still in progress or incomplete
+	FinishReasonNull FinishReason = "null"
+)
